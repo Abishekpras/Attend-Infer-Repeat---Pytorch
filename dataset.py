71b12287fe48722030c46e764c839dbe4b04a1ee
Abishek Prasanna <abisheksamsung@gmail.com>
2018-07-22 23:07:21 -0400

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,24 @@
+from sklearn.preprocessing import MultiLabelBinarizer
+from torch.utils.data import Dataset, DataLoader
+
+class MultiMNIST_Dataset(Dataset):
+
+    def __init__(self, X, y, transform=None):
+    
+        self.mlb = MultiLabelBinarizer()
+        self.transform = transform
+
+        self.X = X
+        self.y = self.mlb.fit_transform(y).astype(np.float32)
+
+    def __getitem__(self, index):
+        img = self.X[index]
+        if self.transform is not None:
+            img = self.transform(img)
+        
+        label = torch.from_numpy(self.y[index])
+        return img, label
+
+    def __len__(self):
+        return len(self.X)
+        
